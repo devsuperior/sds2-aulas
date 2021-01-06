82843d443b2d4d81d53e8defb438b6822586446a
Washington Soares <washingtonudi@gmail.com>
2021-01-06 09:34:17 -0300

    Finish front-web

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -11,11 +11,11 @@ function Home() {
           <div className="home-actions">
             <h1 className="home-title">
               Faça seu pedido <br /> que entregamos <br /> pra você!!!
-          </h1>
+            </h1>
             <h3 className="home-subtitle">
               Escolha o seu pedido e em poucos minutos <br />
-            levaremoss na sua porta
-          </h3>
+              levaremoss na sua porta
+            </h3>
             <Link to="/orders" className="home-btn-order">
               FAZER PEDIDO
             </Link>
